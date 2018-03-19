a9aff5bed329f7d61a15702142e410491565194e
sthadhlani <sthadhlani@gmail.com>
2018-03-18 17:12:05 -0700

    Made changes, instead of box follow the cursor

--- src/cam_capture.py
+++ src/cam_capture.py
@@ -34,43 +34,16 @@ def desktop_drawing(facebox_var,eyebox_var):
     .reshape((printscreen_pil.size[1],printscreen_pil.size[0],4))
     keyboard = cv2.imread('mac_keyboard.jpg',0)
     #print keyboard.shape
-    x,y = 50,50
+    x,y = 25,25
     start_time = time.time()
     training_start = time.time()
     flipflopX = True
     yMove = 0
     while(True):
-        if test == True:
-            tempScreen = np.copy(printscreen_numpy)
-            cv2.namedWindow('window', cv2.WND_PROP_FULLSCREEN)
-            cv2.setWindowProperty('window', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-            cv2.rectangle(tempScreen,(x,y),(x+150,y+150),(0,255,0),2)
-            cv2.imshow('window',tempScreen)
-            if  time.time()-start_time > 1:
-                if(x > 1250):
-                    flipflopX = False
-                if(flipflopX==True):
-                    x = x + 75
-                else:
-                    x = x -75
-                    if yMove == 0:
-                        y = y + 75
-                    elif yMove == -1:
-                        y = y - 75
-                    if x <=250:
-                        if yMove == 0:
-                            yMove = -1
-                        else:
-                            yMove = 0
-                        flipflopX = True
-                        start_time = time.time()
-        else:
-            cv2.imshow('img',img)
-            #pyautogui.moveTo(deltaX, deltaY)
         ret, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
+        
         for (x1,y1,w,h) in faces:
             roi_gray = gray[y1:y1+h, x1:x1+w]
             roi_color = img[y1:y1+h, x1:x1+w]
@@ -86,17 +59,49 @@ def desktop_drawing(facebox_var,eyebox_var):
                     eyebox_var[i] = facebox(facebox_var.x+ex,facebox_var.y+ey,ew,eh)
                     i+=1
         verify_face(img,facebox_var,eyebox_var)
-        k = cv2.waitKey(25) & 0xFF
+        
+        if test == True:
+            tempScreen = np.copy(printscreen_numpy)
+            cv2.namedWindow('window', cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty('window', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+            #cv2.rectangle(tempScreen,(x,y),(x+150,y+150),(0,255,0),2)
+            pyautogui.moveTo(x, y)
+            cv2.imshow('window',tempScreen)
+            if  time.time()-start_time > 1:
+                if(x > 1250):
+                    flipflopX = False
+                if(flipflopX==True):
+                    x = x + 25
+                else:
+                    x = x -25
+                    if yMove == 0:
+                        y = y + 15
+                    elif yMove == -1:
+                        y = y - 15
+                    if x <=250:
+                        if yMove == 0:
+                            yMove = -1
+                        else:
+                            yMove = 0
+                        flipflopX = True
+                        start_time = time.time()
+            #CNN_TRAIN(roi_color, eyebox_var,x,y)
+        else:
+            cv2.imshow('img',img)
+            #deltaX,deltaY = CNN_PREDICT(roi_color,eyebox_var)
+            #pyautogui.moveTo(deltaX, deltaY)
+
+        k = cv2.waitKey(2) & 0xFF
         if (k == ord('q') ):
             cv2.destroyAllWindows()
             break
-        elif (time.time()-training_start>10):
-            cv2.destroyWindow('window')
-            test = False
-        elif k == 115:
+        elif k == ord('s'):
             colorImage = time.strftime("%Y%m%d-%H%M%S")+'.png'
             cv2.imwrite(colorImage,roi_color)
             print colorImage
+        elif (time.time()-training_start>60):
+            cv2.destroyWindow('window')
+            test = False
 
 
 def verify_face(img,facebox_var,eyebox_var):
